fd4b7df35436746010b606feda8a08a45761fbef
devoltamp <devparmar2306@gmail.com>
2026-04-17 00:22:40 +0530

    initial differentiation

---
+++ differentiation.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+time=np.linspace(0,60e-3,600)
+Ts=20e-3/200
+f=50
+signal= 10*np.sin(2*np.pi*f*time)
+N=len(signal)
+d_result=np.zeros(N)
+
+
+for i in range(0,N-1):
+    d_result[i+1]=(signal[i+1]-signal[i])/Ts
+
+
+plt.plot(time,signal)
+plt.plot(time,d_result/(2*np.pi*50))
+plt.show()
